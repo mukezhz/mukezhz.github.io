fc247ba10ead37756d2f2619e7ae197d66f3f381
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-12-11 15:33:56 +0545

    feat: integrate Umami analytics with Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import yeskunallumami from "@yeskunall/astro-umami";
+
 export default defineConfig({
   site: "https://mukesh.name.np",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    yeskunallumami({
+      id: "6e145cd2-36ed-425d-bb8e-f59e641a2671",
+    }),
+  ],
 });
